a4ebe1cf312dfb9f8b7b7d74718b01307854052e
mayx17 <martiny@bahnhof.se>
2026-03-09 16:26:45 +0100

    Update EncryptionException.java

--- src/main/java/se/sundsvall/users/utility/EncryptionException.java
+++ src/main/java/se/sundsvall/users/utility/EncryptionException.java
@@ -3,14 +3,14 @@ package se.sundsvall.users.utility;
 import java.io.Serial;
 
 public class EncryptionException extends RuntimeException {
-    @Serial
-    private static final long serialVersionUID = 1L;
+	@Serial
+	private static final long serialVersionUID = 1L;
 
-    public EncryptionException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+	public EncryptionException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
 
-    public EncryptionException(final String message) {
-        super(message);
-    }
+	public EncryptionException(final String message) {
+		super(message);
+	}
 }
